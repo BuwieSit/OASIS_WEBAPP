8c4982aab10f1630e7d1b8d1a60c9328ae00c014
BuwieSit <santosbuhawie28@gmail.com>
2026-02-09 04:31:51 +0800

    landing page changes

--- frontend/src/landingPage.jsx
+++ frontend/src/landingPage.jsx
@@ -6,7 +6,9 @@ import LandingScreen from './layouts/landingScreen.jsx';
 import fallbackImg from './assets/fallbackImage.jpg';
 import { LogIn } from 'lucide-react';
 import { Link } from 'react-router-dom';
-
+import { ViewModal } from './components/popupModal.jsx';
+import { useState } from 'react';
+import { TutorialCard } from './utilities/card';
 export function HoverContainer({ children }) {
     return (
         <>
@@ -16,11 +18,20 @@ export function HoverContainer({ children }) {
         </>
     )
 }
+
 export default function LandingPage() {
+
+    const [openView, setOpenView] = useState(false);
 
     return(
         <>
             <LandingScreen>
+                <ViewModal 
+                    visible={openView}
+                    onClose={() => setOpenView(false)}
+                    isVideo={true}
+                    resourceTitle="What is OASIS?"
+                />
                 <div className='relative w-full p-5 flex flex-row justify-center items-center gap-10'>
                     <div className='flex flex-row justify-evenly items-center p-3 opacity-100 bg-oasis-blue w-150 aspect-video shadow-[4px_4px_2px_rgba(0,0,0,0.5)] '>   
                         <Link to="/access" className='w-full h-full flex flex-col items-center justify-center gap-5 cursor-pointer transition duration-150 ease-in-out hover:bg-white z-100'> 
@@ -32,17 +43,20 @@ export default function LandingPage() {
                 </div>
                 <UpperWave/>
                 <div className='w-full min-h-150 h-auto pb-5 pt-5 bg-oasis-blue flex flex-wrap flex-col items-center justify-center'>
-                    <Title text="What is OASIS?"/>
-                    <Subtitle text="OJT Administration Support, and Information System is your all-in-one platform for managing OJT requirements, announcements, and host establishment information. Explore the cards below to learn more!"/>
+                     <section className='w-[50%] flex flex-col gap-2 mt-10'>
+                        <Title text="What is OASIS?"/>
+                        <Subtitle isCenter={true} size={'text-[0.9rem]'} text="OJT Administration Support, and Information System is your all-in-one platform for managing OJT requirements, announcements, and host establishment information. Explore the cards below to learn more!"/>
+                    </section>
+                    
 
                     <div className="w-[80%] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 pt-10 pb-10 justify-center place-items-center">
 
-                        <CustomCard title={"Purpose"} desc={"OASIS aims to simplify and digitize internship management — making information, forms, and updates easily accessible for students and coordinators."}></CustomCard>
-                        <CustomCard title={"Announcements"} desc={"Never miss an important deadline or announcement. This page keeps students informed of all OJT-related updates in real time."}></CustomCard>
-                        <CustomCard title={"Downloadable"} desc={"Access all internship documents in one place — including MOAs, accomplishment sheets, evaluation forms, and consent letters."}></CustomCard>
-                        <CustomCard title={"HTE Dashboard"} desc={"View all partner HTEs, track their MOA validity, and monitor which establishments are active or due for renewal."}></CustomCard>
-                        <CustomCard title={"OJT Hub"} desc={"OASIS aims to simplify and digitize internship management — making information, forms, and updates easily accessible for students and coordinators."}></CustomCard>
-                        <CustomCard title={"ORBI"} desc={"ORBI serves as the digital companion of every OASIS user — ensuring no question goes unanswered and every student receives timely, reliable, and friendly support throughout their internship journey."}></CustomCard>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
+                        <TutorialCard onClick={() => setOpenView(true)}/>
                     </div>
                 </div>
                 <LowerWave/>
