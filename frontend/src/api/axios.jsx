ffc58332936cf6b3f845395d2677cf17835ab7f2
Vincent Pagarigan <johnvincentpagarigan@gmail.com>
2026-03-11 10:35:52 +0800

    READY FOR DEPLOYMENT

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,8 +1,31 @@
+// import axios from "axios";
+// import { getToken } from "./token";
+
+// const api = axios.create({
+//   baseURL: "http://127.0.0.1:5001",
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = getToken();
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
+//DEPLOYMENT
+
+
 import axios from "axios";
 import { getToken } from "./token";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5001",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
--- orbi/run.py
+++ orbi/run.py
@@ -1,6 +1,16 @@
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5050)
+
+#DEPLOYMENT VERSION
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    port = int(os.getenv("PORT", 5050))
+    app.run(host="0.0.0.0", port=port, debug=False)
