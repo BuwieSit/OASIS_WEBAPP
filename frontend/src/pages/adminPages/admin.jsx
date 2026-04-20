256ebe969c8db345ddde99e72334bfa26e4d060c
Vincent Pagarigan <johnvincentpagarigan@gmail.com>
2026-04-20 10:20:44 +0800

    Final Final Final Revert Back

--- frontend/src/pages/adminPages/admin.jsx
+++ frontend/src/pages/adminPages/admin.jsx
@@ -264,14 +264,12 @@ export default function Admin() {
                         ribbonColor={"bg-yellow-500"}
                         cardTitle="Total MOAs"
                         cardIcon={<Book color="#377268" />}
-                        cardValue={
-                            dashboardLoading ? "Loading..." :
+                        cardNumber={
+                            loadingDashboard ? <SvgLoader size={20} /> :
                             dashboardError ? "-" :
-                            dashboard?.metrics
-                                ? (dashboard?.metrics?.total_active_moas ?? 0) +
-                                (dashboard?.metrics?.total_expired_moas ?? 0)
-                                : "-"
-                            }
+                            dashboard?.metrics?.total_active_moas + 
+                            dashboard?.metrics?.total_expired_moas ?? "-"
+                        }
                         cardDate={
                             dashboard?.last_updated
                                 ? new Date(dashboard.last_updated).toLocaleDateString()
@@ -525,5 +523,3 @@ export default function Admin() {
         </AdminScreen>
     );
 }
-
-
